3177df80c8e37f9317a317fd601499bd6d5530b7
Manoj-K-Sarkar <wakeupmanoj123@gmail.com>
2020-05-04 19:55:36 +0530

    Add touchmove online nextpage && add navbar

--- app.js
+++ app.js
@@ -34,17 +34,17 @@ function init() {
         const nextRight = nextPage.querySelector(".hero-right");
         const nextText = nextPage.querySelector(".details");
         const body = document.querySelector("body");
-        
+
 
 
         const ti = new TimelineMax({
-            onStart: function(){
-                dots.forEach((dot)=>{
+            onStart: function () {
+                dots.forEach((dot) => {
                     dot.style.pointerEvents = "none";
                 });
             },
-            onComplete: function(){
-                dots.forEach((dot)=>{
+            onComplete: function () {
+                dots.forEach((dot) => {
                     dot.style.pointerEvents = "all";
                 });
             }
@@ -55,8 +55,8 @@ function init() {
         ti.fromTo(currentLeft, 0.3, { y: "-10%" }, { y: "-100%" })
             .fromTo(currentRight, 0.3, { y: "10%" }, { y: "-100%" }, "-=0.2")
             .to(body, 0.3, { backgroundImage: backgroundColors[pageNumber] })
-            .fromTo(currentPage, 0.3, { opacity: 1, pointerEvents: "all" }, { opacity: 0, pointerEvents: "none" })
-            .fromTo(nextPage, 0.3, { opacity: 0, pointerEvents: "none" }, { opacity: 1, pointerEvents: "all" })
+            .fromTo(currentPage, 0.3, { opacity: 1, display: "grid" }, { opacity: 0, display: "none" })
+            .fromTo(nextPage, 0.3, { opacity: 0, display: "none" }, { opacity: 1, display: "grid" })
             .fromTo(nextLeft, 0.3, { y: "-100%" }, { y: "-10%" }, "-=0.4")
             .fromTo(nextRight, 0.3, { y: "-100%" }, { y: "10%" }, "-=0.2")
             .set(nextLeft, { clearProps: "all" })
@@ -64,7 +64,6 @@ function init() {
             .fromTo(nextText, 0.3, { opacity: 0 }, { opacity: 1 });
 
     }
-
 
     // Mouse Scroll Handler
 
@@ -82,21 +81,45 @@ function init() {
     }
 
     document.addEventListener("wheel", throttle(scrollChange, 1500));
-    // document.addEventListener("touchmove", throttle(scrollChange, 1500));
+    document.addEventListener("touchmove", throttle(scrollChange, 1500));
 
-    function scrollChange(e){
-        console.log(e);
-        
+    function scrollChange(e) {
+        // console.log(e);
+
         let scrollPageNumber = currentPageNumber;
 
-        if(e.deltaY < 0) scrollPageNumber= ((scrollPageNumber-1) + pages.length)%pages.length; 
-        else scrollPageNumber = (scrollPageNumber+1) % pages.length;
+        if (e.deltaY < 0) scrollPageNumber = ((scrollPageNumber - 1) + pages.length) % pages.length;
+        else scrollPageNumber = (scrollPageNumber + 1) % pages.length;
 
         const dot = dots[scrollPageNumber];
         changeDots(dot);
-        changePage(scrollPageNumber);    
+        changePage(scrollPageNumber);
     }
 
+
+    //Navbar Handler
+
+    const menu = document.querySelector(".menu");
+    const navbar = document.querySelector(".navbar");
+    const hiddenmenu = document.querySelector(".hidden");
+    const nav = document.querySelector("nav");
+
+    const tl = new TimelineMax({ paused: true, reversed: true });
+
+    tl.to(navbar, 0.3, { opacity: 1, y: "0%" })
+        .to(nav, 0.1, {backgroundColor: "black"} , "-=0.2");
+
+    menu.addEventListener("click", function () {
+        tl.play();
+        menu.classList.add("hidden");
+        hiddenmenu.classList.remove("hidden");
+    });
+
+    hiddenmenu.addEventListener("click", function() {
+        tl.reverse();
+        menu.classList.remove("hidden");
+        hiddenmenu.classList.add("hidden");
+    });
 }
 
 
